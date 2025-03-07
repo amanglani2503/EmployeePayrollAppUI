f6f5a22e017ce51553a8990b27ed91e3e73a2ad1
amanglani2503 <ambermanglani8871@gmail.com>
2025-03-07 13:37:09 +0530

    [Amber] Add. Designed component to view list of all employees

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { AddEmployeeComponent } from './pages/add-employee/add-employee.component';
+import { EmployeeListComponent } from './pages/employee-list/employee-list.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'add-employee', component: AddEmployeeComponent}
+  { path: 'add-employee', component: AddEmployeeComponent},
+  { path: 'employee-list', component: EmployeeListComponent}
 ];
---
+++ src/app/pages/employee-list/employee-list.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common'; 
+
+@Component({
+  selector: 'app-employee-list',
+  imports: [RouterModule, CommonModule],
+  templateUrl: './employee-list.component.html',
+  styleUrl: './employee-list.component.css'
+})
+export class EmployeeListComponent {
+  employees = [
+    { 
+      name: 'Amarpa Kumar', 
+      gender: 'Female', 
+      departments: ['Sales', 'HR', 'Finance'], 
+      salary: '₹ 10,000', 
+      startDate: '29 Oct 2019', 
+      image: 'assets/dp1.jpeg' 
+    },
+    { 
+      name: 'Mohammad Shaikh', 
+      gender: 'Female', 
+      departments: ['Sales', 'HR', 'Finance'], 
+      salary: '₹ 10,000', 
+      startDate: '29 Oct 2019', 
+      image: 'assets/dp2.jpeg'  
+    },
+    { 
+      name: 'Bubere Qais', 
+      gender: 'Male', 
+      departments: ['Sales', 'HR', 'Finance'], 
+      salary: '₹ 10,000', 
+      startDate: '29 Oct 2019', 
+      image: 'assets/dp3.jpeg'
+    }
+  ];
+  
+}
