5daca24ffdd3d2bb07fedc6caaa7ff8fc420e4bf
amanglani2503 <ambermanglani8871@gmail.com>
2025-03-07 13:21:25 +0530

    [Amber] Add. Designed add-employee component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { AddEmployeeComponent } from './pages/add-employee/add-employee.component';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent }
+  { path: '', component: HomeComponent },
+  { path: 'add-employee', component: AddEmployeeComponent}
 ];
